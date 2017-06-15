b663895504708617a061deba770c72b8996093e7
Shana Austin <shanaaustin00@gmail.com>
2017-06-15 14:03:01 +0100

    Third commit

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,15 +1,48 @@
 package controllers
 
 import org.scalatestplus.play._
-import play.api.test._
-import play.api.test.Helpers._
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{status, _}
+import play.api.mvc.{Action, Controller}
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
+class WelcomeController extends Controller {
+  def welcome() = Action {
+    Ok
+  }
+}
+
+
+
+class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+  "WelcomeController GET" should {
+    "return a successful response" in {
+      val controller = new WelcomeController
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      status(result) mustBe OK
+    }
+    "respond to the /welcome url" in {
+      // Need to specify Host header to get through AllowedHostsFilter
+      val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
+      val home = route(app, request).get
+      status(home) mustBe OK
+    }
+    "return some html" in {
+      val controller = new WelcomeController
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      contentType(result) mustBe Some("text/html")
+    }
+
+    "say hello and have a title" in {
+      val controller = new WelcomeController
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      contentAsString(result) must include ("<h1>Hello!</h1>")
+      contentAsString(result) must include ("<title>Welcome!</title>")
+    }
+  }
+}
+
+
 class HomeControllerSpec extends PlaySpec with OneAppPerTest {
 
   "HomeController GET" should {
